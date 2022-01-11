21bcf89ae13cad5cb31859ad57b8d7759aea2c48
William Galvao <wsg.online@gmail.com>
2022-01-11 12:15:50 -0300

    added home main content / need to adjust responsive img

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,21 @@
+import cleanprod from "../assets/images/cleaning-products.jpg"
+
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
   <Navbar />
-  <h1>Conteúdo</h1>
+  <div className="row d-flex align-items-center">
+    <div className="col-md-4">
+<p className="ps-5 pt-5" style={{fontSize: "26px"}}>A <strong>Casa da Faxina</strong> reúne as melhores marcas da limpeza e conservação.</p>
+<p className="ps-5" style={{fontSize: "26px"}}>Seja na nossa loja física ou através da nossa entrega rápida, oferecemos um atendimento personalizado para que você encontre o produto mais adequado à sua atividade.</p>
+<p className="ps-5" style={{fontSize: "26px"}}>Temos também planos especiais para a sua empresa ou condomínio.</p>
+    </div>
+    <div className="col-md-8">
+  <img src={cleanprod} alt="produtos de limpeza"/>
+  </div>
+  </div>
   </div>
   )
 }
