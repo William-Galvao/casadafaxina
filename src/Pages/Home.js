31ed749a3b52ae8d97e031e2911a01ea67d64320
William Galvao <wsg.online@gmail.com>
2022-01-12 10:01:34 -0300

    done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,30 +1,33 @@
 import cleanprod from "../assets/images/cleaning-products.jpg";
 import whats from "../assets/images/whatsapp.png";
-
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import FadeIn from "react-fade-in/lib/FadeIn";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <a rel="noreferrer" target="_blank" href="https://wa.me/+5521979677478">
+      <a rel="noreferrer" target="_blank" href="https://wa.me/+552199931-3595">
         <img src={whats} alt="Whatsapp" className="whats-icon" />
       </a>
-      <div className="row d-flex align-items-center pt-5">
+      <div className="row d-flex align-items-center pt-5 pb-5">
         <div className="col-md-4">
           <hr />
-          <p className="ps-5 pt-5 pe-3 p-font">
-            A <strong>Casa da Faxina</strong> reúne as melhores marcas da
-            limpeza e conservação.
-          </p>
-          <p className="ps-5 pe-3 p-font">
-            Seja na nossa loja física ou através da nossa entrega rápida,
-            oferecemos um atendimento personalizado para que você encontre o
-            produto mais adequado à sua atividade.
-          </p>
-          <p className="ps-5 pe-3 pb-5 p-font">
-            Temos também planos especiais para a sua empresa ou condomínio.
-          </p>
+          <FadeIn delay="400" transitionDuration="2000">
+            <p className="ps-5 pt-5 pe-3 p-font-main">
+              A <strong>Casa da Faxina</strong> reúne as melhores marcas da
+              limpeza e conservação.
+            </p>
+            <p className="ps-5 pe-3 p-font-main">
+              Seja na nossa loja física ou através da nossa entrega rápida,
+              oferecemos um atendimento personalizado para que você encontre o
+              produto mais adequado à sua atividade.
+            </p>
+            <p className="ps-5 pe-3 pb-5 p-font-main">
+              Temos também planos especiais para a sua empresa ou condomínio.
+            </p>
+          </FadeIn>
           <hr />
         </div>
         <div className="col-md-8">
@@ -35,6 +38,7 @@ export default function Home() {
           />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
