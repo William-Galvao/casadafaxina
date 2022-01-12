af8baa88ddc4f91f425df99d9bda66a44a379c2c
William Galvao <wsg.online@gmail.com>
2022-01-12 08:18:00 -0300

    changed navbar to simple / added whatsapp link

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,40 @@
-import cleanprod from "../assets/images/cleaning-products.jpg"
+import cleanprod from "../assets/images/cleaning-products.jpg";
+import whats from "../assets/images/whatsapp.png";
 
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
-  <Navbar />
-  <div className="row d-flex align-items-center pt-5">
-    <div className="col-md-4 border-top border-bottom border-2">
-<p className="ps-5 pt-5" style={{fontSize: "26px"}}>A <strong>Casa da Faxina</strong> reúne as melhores marcas da limpeza e conservação.</p>
-<p className="ps-5" style={{fontSize: "26px"}}>Seja na nossa loja física ou através da nossa entrega rápida, oferecemos um atendimento personalizado para que você encontre o produto mais adequado à sua atividade.</p>
-<p className="ps-5 pb-5" style={{fontSize: "26px"}}>Temos também planos especiais para a sua empresa ou condomínio.</p>
+      <Navbar />
+      <a rel="noreferrer" target="_blank" href="https://wa.me/+5521979677478">
+        <img src={whats} alt="Whatsapp" className="whats-icon" />
+      </a>
+      <div className="row d-flex align-items-center pt-5">
+        <div className="col-md-4">
+          <hr />
+          <p className="ps-5 pt-5 pe-3 p-font">
+            A <strong>Casa da Faxina</strong> reúne as melhores marcas da
+            limpeza e conservação.
+          </p>
+          <p className="ps-5 pe-3 p-font">
+            Seja na nossa loja física ou através da nossa entrega rápida,
+            oferecemos um atendimento personalizado para que você encontre o
+            produto mais adequado à sua atividade.
+          </p>
+          <p className="ps-5 pe-3 pb-5 p-font">
+            Temos também planos especiais para a sua empresa ou condomínio.
+          </p>
+          <hr />
+        </div>
+        <div className="col-md-8">
+          <img
+            src={cleanprod}
+            alt="produtos de limpeza"
+            className="img-fluid"
+          />
+        </div>
+      </div>
     </div>
-    <div className="col-md-8">
-  <img src={cleanprod} alt="produtos de limpeza" className="img-fluid"/>
-  </div>
-  </div>
-  </div>
-  )
+  );
 }
